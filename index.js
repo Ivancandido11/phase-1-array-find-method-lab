712bf9751e233867075c37a0e0611854c1458426
Ivan Candido <ivan.candido@powerhrg.com>
2021-05-25 17:03:00 -0400

    Finally Completed

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 
+const findMatchingYear = (element) => element['result'] === 'W'
+    function superbowlWin(records){
+        const year = records.find(findMatchingYear)
+        if (year !== undefined){
+            return year['year']
+    }
+}
